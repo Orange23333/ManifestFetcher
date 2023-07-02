7b69e5aaf595c994b5e0546ff2281fa8430ec6b4
Orange23333viaDS15 <874462262@qq.com>
2023-07-03 02:52:55 +0800

    Update info.

--- ManifestFetcher/Properties/AssemblyInfo.cs
+++ ManifestFetcher/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ManifestFetcher")]
-[assembly: AssemblyDescription("A software to fetch files according to the manifest.")]
+[assembly: AssemblyDescription("https://github.com/Orange23333/ManifestFetcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bisitsoft")]
 [assembly: AssemblyProduct("ManifestFetcher")]
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ManifestFetcher
+
+A software to fetch file list in manifest.
+一个根据清单获取文件的软件。
+
+---
+
+View this repository on [Github](https://github.com/Orange23333/ManifestFetcher)
